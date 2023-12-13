6f8fb478250043dd5a2381cd3303613fd5071a48
Slava Fumin <slavafumin@gmail.com>
2023-12-13 11:15:08 +0300

    fixed the path in the Project (Link)

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Project = ({title, img, slug}) => {
   return (
     <li className="project">
-      <a href={slug}>
+      <Link to={slug}>
         <img src={img} alt="title" className="project__img" />
         <h3 className="project__title">{title}</h3>
-      </a>
+      </Link>
     </li>
   );
 }
