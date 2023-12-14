c5bff58a930ea0bb3eb052c2230efa0821ab037e
Slava Fumin <slavafumin@gmail.com>
2023-12-14 12:40:20 +0300

    .js to jsx, npm run lint -- --fix

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['airbnb-base'],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+};
--- src/components/data/projectList.js
+++ src/components/data/projectList.js
@@ -1,10 +1,9 @@
-import downloader from './../../img/projects/downloader.jpg'
-import task from './../../img/projects/task.jpg'
-import reactSite from './../../img/projects/reactSite.jpg'
-import downloaderBig from './../../img/projects/downloaderBig.jpg'
-import taskBig from './../../img/projects/taskBig.jpg'
-import reactSiteBig from './../../img/projects/reactSiteBig.jpg'
-
+import downloader from '../../img/projects/downloader.jpg';
+import task from '../../img/projects/task.jpg';
+import reactSite from '../../img/projects/reactSite.jpg';
+import downloaderBig from '../../img/projects/downloaderBig.jpg';
+import taskBig from '../../img/projects/taskBig.jpg';
+import reactSiteBig from '../../img/projects/reactSiteBig.jpg';
 
 const projects = {
   'js-tasks': {
@@ -13,15 +12,15 @@ const projects = {
     img: task,
     imgBig: taskBig,
     slug: 'js-tasks',
-    gitHubLink: 'https://github.com'
+    gitHubLink: 'https://github.com',
   },
-  'react': {
+  react: {
     title: 'React site',
     skills: 'React, Node.js, Doker',
     img: reactSite,
     imgBig: reactSiteBig,
     slug: 'react',
-    gitHubLink: 'https://github.com'
+    gitHubLink: 'https://github.com',
   },
   'youtube-app': {
     title: 'Youtube downloader app',
@@ -29,8 +28,8 @@ const projects = {
     img: downloader,
     imgBig: downloaderBig,
     slug: 'youtube-app',
-    gitHubLink: 'https://github.com'
+    gitHubLink: 'https://github.com',
   },
-}
+};
 
-export default projects
+export default projects;
