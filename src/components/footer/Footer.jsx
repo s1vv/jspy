e0481bd4d0c4c902287f764d6ec1675d4e5fd38a
Slava Fumin <slavafumin@gmail.com>
2023-12-15 16:49:52 +0300

    del eslint, original .json

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,9 @@ const Footer = () => {
           <div className="container">
               <div className="footer__wrapper">
                   <ul className="social">
-                      <li className="social__item"><a href="https://github.com/s1vv" target="_blank"><img src={gitHub} alt="Link" /></a></li>
+                      <li className="social__item">
+                        <a href="https://github.com/s1vv" target="_blank" rel="noreferrer">
+                          <img src={gitHub} alt="Link" /></a></li>
                   </ul>
                   <div className="copyright">
                       <p>© 2023 jspy.top</p>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -10,7 +10,10 @@ const Home = () => {
                   <ul className="content-list">
                       <li className="content-list__item">
                         <h2 className="title-2">Отблагодарить можно по ссылке ниже</h2>
-                        <a href="https://donate.qiwi.com/payin/jspy.top" target="_blank" className="btn">Донат</a>
+                        <a href="https://donate.qiwi.com/payin/jspy.top" 
+                          target="_blank" 
+                          rel="noreferrer" 
+                          className="btn">Донат</a>
 
                       </li>
 
